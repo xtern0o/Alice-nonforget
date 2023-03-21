41248c7bb56c24a3417ee51c7b890ca71ecade75
xtern0o <dos30028gmail.com>
2023-03-21 21:03:51 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+import logging
+
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    session = request.json['session']
+    version = request.json['version']
+
+    if session['new']:
+        answer_response = {
+            "response": {
+                'text': 'Вы запустили сценарий "незабывайка".'
+                        ' Вы хотите создать новый сценарий, запустить или удалить существующий?',
+                'tts': 'Приветствую вас в сценарий "незабывайка". Хотите создать сценарий или применить существующий?',
+                'buttons': [
+                    {
+                        "title": "Создать",
+                        "hide": True
+                    },
+                    {
+                        "title": "Запустить",
+                        "hide": True
+                    },
+                    {
+                        "title": "Удалить",
+                        "hide": True
+                    }
+                ],
+                'end_session': False,
+            },
+            "session": session,
+            "version": version
+        }
+        return jsonify(answer_response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

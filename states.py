43ce26c4ef205c933cf18d0d2ed9eb5627656537
itsdenisska <fotiev.d1991@gmail.com>
2023-03-22 16:57:34 +0300

    add database, refactor states, add settings and bagofix

--- states.py
+++ states.py
@@ -1,24 +1,15 @@
 class States:
     def __init__(self):
-        self.CREATING_SCENARY = 0
-        self.USE_EXIST_SCENARY = 0
-        self.DELETE_SCENARY = 0
+        self.current_state = '*'
 
-    def get_creating_state(self):
-        return self.CREATING_SCENARY
+    def get_state(self):
+        return self.current_state
 
-    def get_using_state(self):
-        return self.USE_EXIST_SCENARY
+    def set_creating_state(self):
+        self.current_state = 'CREATING_SCENARY'
 
-    def get_delete_state(self):
-        return self.DELETE_SCENARY
+    def set_using_state(self):
+        self.current_state = 'USE_EXIST_SCENARY'
 
-    def set_creating_state(self, value):
-        self.CREATING_SCENARY = value
-
-    def set_using_state(self, value):
-        self.USE_EXIST_SCENARY = value
-
-    def set_delete_state(self, value):
-        self.DELETE_SCENARY = value
-
+    def set_delete_state(self):
+        self.current_state = 'DELETE_SCENARY'

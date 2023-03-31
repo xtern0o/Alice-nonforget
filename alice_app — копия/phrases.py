e0661d337ee4d0eaff9e7b8e22fb22523f48fcee
Saveliy Akoru <74486667+akorutant@users.noreply.github.com>
2023-03-31 19:37:20 +0300

    it cube SQLITE REWRITING and тэто аргпарс

---
+++ alice_app — копия/phrases.py
@@ -0,0 +1,22 @@
+from json import loads
+from random import choice
+
+with open("tourist.json", mode="r", encoding="utf-8") as json_file:
+    f = json_file.read()
+PHRASES = loads(f)
+
+
+def get_phrase(state: str, stage: str) -> dict:
+    return PHRASES["state"][state][stage]
+
+
+def get_error_phrase(arg=None):
+    if not arg:
+        return choice(
+            [
+                PHRASES["errors"]["first_error"],
+                PHRASES["errors"]["second_error"],
+                PHRASES["errors"]["third_error"]
+            ]
+        )
+    return PHRASES["errors"][arg]
